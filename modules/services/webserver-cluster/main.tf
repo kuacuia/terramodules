988bc5f45e8f696e4005e817f2880ebedf03fc00
frank fong <frankf@ticketek.com.au>
2018-09-03 11:48:34 +0000

    deleted remote dbx

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -104,12 +104,12 @@ resource "aws_security_group_rule" "allow_all_outbound" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
-  config {
-    bucket = "${var.db_remote_state_bucket}"
-    key    = "${var.db_remote_state_key}"
-    region = "ap-southeast-2"
-    #dynamodb_table = "${var.dynamodb_name}"
-  }
-}
+#data "terraform_remote_state" "db" {
+#  backend = "s3"
+#  config {
+#    bucket = "${var.db_remote_state_bucket}"
+#    key    = "${var.db_remote_state_key}"
+#    region = "ap-southeast-2"
+#    #dynamodb_table = "${var.dynamodb_name}"
+#  }
+#}
